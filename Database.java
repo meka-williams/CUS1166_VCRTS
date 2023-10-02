ae83bdc3dca833a2a3fd5501866b9b404e6227e1
Avery Lagredelle <averylagredelle@gmail.com>
2023-10-02 15:52:15 -0400

    Minor formatting changes

---
+++ Database.java
@@ -0,0 +1,57 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Database {
+
+// 	public static void main(String[] args) {
+	
+//     Scanner scanner = new Scanner(System.in);
+// 		List<Client> client = new ArrayList<>();
+
+
+// 		while (true) {
+// 			System.out.println("Enter user information or type 'quit' to exit:");
+			
+// 			String quit = scanner.nextLine();
+
+// 			if (quit.equalsIgnoreCase("quit")) {
+// 				break;
+// 			}
+
+// 			System.out.print("First Name: ");
+// 			String firstName = scanner.nextLine();
+
+// 			System.out.print("Last Name: ");
+// 			String lastName = scanner.nextLine();
+
+// 			System.out.print("Email: ");
+// 			String Email = scanner.nextLine();
+
+// 			System.out.print("License Plate: ");
+// 			String licensePlate = scanner.nextLine();
+
+// 			Client user = new Client(firstName, lastName, Email, licensePlate);
+// 			client.add(user);	
+// 	}
+
+// 	scanner.close();
+// 	  // Save user information to a text file
+// 		try (BufferedWriter writer = new BufferedWriter(new FileWriter("clientInformation.txt"))) {
+// 			for (Client user : client) {
+// 					writer.write(user.toString());
+// 					writer.newLine();
+// 			}
+// 			System.out.println("User information saved to clientInformation.txt");
+// 	} catch (IOException e) {
+// 			System.err.println("Error writing to file: " + e.getMessage());
+// 	}
+//   }
+
+	private File database = new File("Database.txt");
+	private String data;
+}
--- client.java
+++ client.java
@@ -1,45 +1,50 @@
 public class Client {
 
-	
-  String firstName;
-  String lastName;
-  String Email;
-  String licensePlate;
-   public Client(String firstName, String lastName, String Email , String licensePlate) {
-          this.firstName = firstName;
-          this.lastName = lastName;
-          this.Email = Email;
-          this.licensePlate = licensePlate;
-      }
-  public String getFirstName() {
-    return firstName;
-  }
-  public void setFirstName(String firstName) {
+	private String firstName;
+  private String lastName;
+  private String email;
+  private String licensePlate;
+  
+  public Client(String firstName, String lastName, String email , String licensePlate) {
     this.firstName = firstName;
-  }
-  public String getLastName() {
-    return lastName;
-  }
-  public void setLastName(String lastName) {
     this.lastName = lastName;
-  }
-  public String getEmail() {
-    return Email;
-  }
-  public void setEmail(String email) {
-    Email = email;
-  }
-  public String getLicensePlate() {
-    return licensePlate;
-  }
-  public void setLicensePlate(String licensePlate) {
+    this.email = email;
     this.licensePlate = licensePlate;
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
 
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getLicensePlate() {
+    return licensePlate;
+  }
+
+  public void setLicensePlate(String licensePlate) {
+    this.licensePlate = licensePlate;
+  }
+  
   public String toString() {
-    return "Client Information{" + "/n First Name: " + firstName + "/n Last Name: " + lastName + "/n Email: " + Email + "/n License Plate: " + licensePlate + '}';
+    return "Client Information{" + "/n First Name: " + firstName + "/n Last Name: " + lastName + "/n Email: " + email + "/n License Plate: " + licensePlate + '}';
+  }
 }
-
-    }
