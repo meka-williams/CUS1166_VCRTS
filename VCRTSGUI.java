6e665badd819c18204055a004c641b80be86fefe
Matthew Frankowski <matthew.frankowski21@my.stjohns.edu>
2023-10-03 22:43:59 -0400

    Added Functionality to createCarRentalPage

--- VCRTSGUI.java
+++ VCRTSGUI.java
@@ -306,11 +306,20 @@ public class VCRTSGUI {
       makeSubPanel.add(makeLable, BorderLayout.WEST);
       makeSubPanel.add(make, BorderLayout.EAST);
 
+      JPanel modelSubPanel = new JPanel();
+      JLabel modelLable = new JLabel("Enter Model of Vehicle");
+      JTextField model = new JTextField(20);
+
+      modelSubPanel.setLayout(new BorderLayout(5,0));
+      modelSubPanel.add(modelLable, BorderLayout.WEST);
+      modelSubPanel.add(model, BorderLayout.EAST);
+
       JButton submit = new JButton("Submit Job");
       submit.addActionListener(verifier);
 
       carRentalPanel.add(header);
       carRentalPanel.add(makeSubPanel);
+      carRentalPanel.add(modelSubPanel);
       carRentalPanel.add(submit);
       frame.add(carRentalPanel, CREATE_CAR_RENTAL_PAGE_NAME);
       screens.add(CREATE_CAR_RENTAL_PAGE_NAME);
