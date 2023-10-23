34e11aee141ac309b5027d18127f670eee00a3ab
Krish Patel <krish.patel21@stjohns.edu>
2023-10-23 13:21:06 -0400

    controller class implemented

---
+++ Controller.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Controller {
+  private ArrayList<Car> vehiclesArrayList;
+  private ArrayList<Job> jobs;
+  //private;
+
+
+
+public static void assignJob() {
+
+}
+}
